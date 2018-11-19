c90ac04476380314e3ceb109ba883ae797d7a85a
Thomas Opsomer <thomasopsomer.enpc@gmail.com>
2018-11-19 14:16:44 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# MongoDB S3 Backup Docker image
+
+Based on https://github.com/agmangas/mongo-backup-s3.
+With some modification from https://gist.github.com/caraboides/7679bb73f4f13e36fc2b9dbded3c24c0
+And simplification in Dockerfile :)
+
+...
+
+Docker image that performs periodic backups on a MongoDB database and then uploads the results to Amazon S3.
+
+Based on the [dbader/schedule](https://github.com/dbader/schedule) Python scheduling package.
+
+## Configuration
+
+The following table describes the available configuration environment variables.
+
+Name | Description | Default
+--- | --- | ---
+`MONGO_HOST` | MongoDB instance hostname | *Required*
+`MONGO_PORT` | MongoDB database name | *Required*
+`MONGO_DB` | MongoDB database name | *Required*
+`MONGO_USERNAME` | MongoDB database name | *Required*
+`MONGO_PASSWORD` | MongoDB database name | *Required*
+`S3_FOLDER` | Amazon S3 bucket name | *Required*
+`AWS_ACCESS_KEY_ID` | Amazon AWS access key | *Required*
+`AWS_SECRET_ACCESS_KEY` | Amazon AWS secret | *Required*
+`BACKUP_INTERVAL` | Interval between each backup (days) | `1`
+`BACKUP_TIME` | Hour of the day at which the backup will be executed | `2:00`
+`DATE_FORMAT` | Date format string used as the suffix of the backup filename | `%Y%m%d-%H%M%S`
+`FILE_PREFIX` | Prefix of the backup filename | `backup-`
+
+## Example
+
+The following command starts a *mongo-s3-backup* container that will stay in the background uploading backups of the *testdb* database on the *my-mongo-host* MongoDB instance every day at 2:00. The backups will be uploaded to an S3 bucket named *my-s3-bucket*:
+
+```
+docker run -d --name mongo_backups \
+	-e MONGO_HOST=localhost -e MONGO_PORT=27017 \
+	-e MONGO_USERNAME=root -e MONGO_PASSWORD=test \
+	-e MONGO_DB=data \
+	-e S3_FOLDER=s3://asgard-db-backup/mongo/ \
+	-e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+	-e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+	-e BACKUP_INTERVAL=1  \
+	-e BACKUP_TIME=19:00 \
+	asgard/mongo-backup-s3
+
+docker run -d --name mongo_backups --net asgardapi_asgard_net --env-file .env \
+	asgard/mongo-backup-s3
+
+```
---
+++ backup.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Script that runs a MongoDB backup job periodically according
+to the interval defined by the INTERVAL_NAME environment variable.
+"""
+
+import time
+import datetime
+import subprocess
+import functools
+import traceback
+import os
+import schedule
+import begin
+
+
+ENV_BACKUP_INTERVAL = "BACKUP_INTERVAL"
+ENV_BACKUP_TIME = "BACKUP_TIME"
+TIME_FORMAT = "%H:%M"
+
+
+def catch_exceptions(job_func):
+    @functools.wraps(job_func)
+    def wrapper(*args, **kwargs):
+        try:
+            job_func(*args, **kwargs)
+        except Exception:
+            print(traceback.format_exc())
+
+    return wrapper
+
+
+@catch_exceptions
+def backup_job():
+    print("Executing backup at {}".format(datetime.datetime.now().isoformat()))
+    date = datetime.datetime.today().strftime(os.environ["DATE_FORMAT"])
+    dbs = os.environ["MONGO_DB"].split(",")
+    storage = os.environ["MONGO_BACKUP_STORAGE"]
+    for db in dbs:
+        path = (
+            os.environ["BUCKET"]
+            + os.environ["FILE_PREFIX"]
+            + db
+            + "-"
+            + date
+            + ".dump.gzip"
+        )
+        args = {
+            "host": os.environ.get("MONGO_HOST"),
+            "port": os.environ.get("MONGO_PORT"),
+            "user": os.environ.get("MONGO_USERNAME"),
+            "pwd": os.environ.get("MONGO_PASSWORD"),
+            "db": os.environ.get("MONGO_DB"),
+            "path": path,
+        }
+        if storage == "s3":
+            cmd_ = (
+                "mongodump -h {host} --port {port} -u {user} -p {pwd} --db {db} "
+                + "--gzip --archive --authenticationDatabase admin | "
+                + "aws s3 cp - {path}"
+            )
+        elif storage == "gs":
+            cmd_ = (
+                "mongodump -h {host} --port {port} -u {user} -p {pwd} --db {db} "
+                + "--gzip --archive --authenticationDatabase admin | "
+                + "gsutil -m cp - {path}"
+            )
+        else:
+            raise ValueError("Storage needs to be in ['s3', 'gs']")
+
+        backup_result = subprocess.check_output([cmd_.format(**args)], shell=True)
+        print(backup_result)
+    print("Backup finished at {}".format(datetime.datetime.now().isoformat()))
+
+
+@begin.start(auto_convert=True)
+def main(test=False):
+    print(
+        "Starting periodic MongoDB backup at {}".format(
+            datetime.datetime.now().isoformat()
+        )
+    )
+
+    try:
+        interval_days = int(os.environ.get(ENV_BACKUP_INTERVAL))
+    except Exception:
+        raise ValueError("Undefined or invalid var: {}".format(ENV_BACKUP_INTERVAL))
+
+    try:
+        backup_time = os.environ.get(ENV_BACKUP_TIME)
+        datetime.datetime.strptime(backup_time, TIME_FORMAT)
+    except Exception:
+        raise ValueError("Undefined or invalid var: {}".format(ENV_BACKUP_TIME))
+
+    print("Executing backups every {} day/s at {}".format(interval_days, backup_time))
+
+    if test:
+        backup_job()
+    else:
+        schedule.every(interval_days).days.at(backup_time).do(backup_job)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
